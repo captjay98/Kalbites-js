856e7930bc2aca7376a3cd1070b25b7c3e898998
captjay98wsl <captjay98@gmail.com>
2023-03-19 06:44:02 +0100

    fixed routes

--- routes/items.js
+++ routes/items.js
@@ -1,27 +1,23 @@
 import express from "express";
 import {
-  cake_index, muffins_index, cookies_index,
-  rolls_index, cups_index, brownies_index
+  cake_index, muffins_index, cookies_index, rolls_index,
+  cups_index, brownies_index, chops_index, cake_detail,
+  order_index_post
 } from "../controllers/itemController.js";
 
 const irouter = express.Router();
 
-
-irouter.get("/", cake_index);
-
-irouter.get("/", muffins_index);
-
-irouter.get("/", cookies_index)
-
-irouter.get("/", rolls_index);
-
-irouter.get("/", cups_index);
-
-irouter.get("/", brownies_index);
-
+irouter.get("/", chops_index);
+irouter.get("/cakes", cake_index);
+irouter.get("/cakes/:id", cake_detail)
+irouter.get("/muffins", muffins_index);
+irouter.get("/cookies", cookies_index)
+irouter.get("/rolls", rolls_index);
+irouter.get("/cups", cups_index);
+irouter.get("/brownies", brownies_index);
+// irouter.get("/order", order_index)
+irouter.post("/", order_index_post)
 // router.patch();
-
 // router.delete();
-
-
+// order_index, 
 export { irouter };
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,14 @@
 import express from "express";
 import {
-  login_index, register_Index,
-  profile_index, order_index
+  login_index, register_Index, profile_index
 } from "../controllers/userController.js"
 
 const urouter = express.Router();
 
 urouter.post("/", login_index);
-
 urouter.post("/", register_Index);
-
 urouter.get("/", profile_index);
-
-urouter.get("/", order_index);
-
-urouter.post("/", order_index);
-
 // urouter.patch();
-
 // urouter.delete();
 
 export { urouter };

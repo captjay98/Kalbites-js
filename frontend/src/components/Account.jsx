b5bcc7ef1ef50c3929f564da8e31b3e913703416
captjay98Zorin <captjay98@gmail.com>
2023-08-14 23:34:01 +0100

    Fetcing and Rendering LIve data

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,42 +1,61 @@
 import InfoBar from "./InfoBar";
+import React, { useEffect, useState, useMemo } from "react";
+import axiosInstance from "../../axiosInstance";
 
 const Account = () => {
   const description = "Profile";
-  const user = {
-    name: "John Doe",
-    username: "johndoe",
-    email: "johndoe@example.com",
-    phone: "123-456-7890",
-    address: "123 Main St, City, State",
-    previousOrders: [
-      { id: 1, product: "Product 1", price: "$10.00" },
-      { id: 2, product: "Product 2", price: "$15.00" },
-      { id: 3, product: "Product 3", price: "$20.00" },
-    ],
+
+  const [user, setUserDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getUserDetails = async () => {
+    try {
+      const response = await axiosInstance.get("/users/account");
+      setUserDetails(response.data.user);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
+
+  useEffect(() => {
+    console.log("Getting User Details", user);
+    getUserDetails();
+  }, []);
+
+  useEffect(() => {
+    console.log("User", user);
+  }, [user]);
+
+  if (loading || !user) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="h-screen myBlue">
       <InfoBar description={description} />
       <div className="container mx-auto py-10">
         <div className="max-w-md mx-auto bg-yellow-500 w-11/12  rounded-3xl shadow-md p-8">
-          <h2 className="text-2xl font-bold text-myBlue mb-4">Profile</h2>
+          <h2 className="text-2xl font-bold text-myBlue mb-4">Customer Details</h2>
           <div className="mb-4">
-            <p className="text-gray-800">Name: {user.name}</p>
-            <p className="text-gray-800">Username: {user.username}</p>
-            <p className="text-gray-800">Email: {user.email}</p>
+            <p className="text-gray-800">Name: {user.firstName}</p>
             <p className="text-gray-800">Phone: {user.phone}</p>
             <p className="text-gray-800">Address: {user.address}</p>
           </div>
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">Previous Orders</h3>
-          <ul className="space-y-2">
-            {user.previousOrders.map((order) => (
-              <li key={order.id} className="text-gray-800">
-                {order.product} - {order.price}
-              </li>
-            ))}
-          </ul>
-        </div>
+          {/* <h3 className="text-lg font-semibold text-gray-800 mb-2">Previous Orders</h3> */}
+          {/* {user.previousOrders.length > 0 ? ( */}
+          {/*   <ul className="space-y-2"> */}
+          {/*     {user.previousOrders.map((order) => ( */}
+          {/*       <li key={order.id} className="text-gray-800"> */}
+          {/*         {order.product} - {order.price} */}
+          {/*       </li> */}
+          {/*     ))} */}
+          {/*   </ul> */}
+          {/* ) : ( */}
+          {/*   <p className="text-gray-800">No previous orders</p> */}
+          {/* )} */}
+        </div>{" "}
       </div>
     </div>
   );
--- frontend/src/components/item.jsx
+++ frontend/src/components/item.jsx
@@ -1,53 +1,32 @@
 import React from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import InfoBar from "./InfoBar";
+import axiosInstance from "../../axiosInstance";
 
 const Item = () => {
   const { category, itemId } = useParams();
-  console.log(category, itemId);
-  let itemName = "";
-  let itemImagePath = "";
-  let itemPriceLabel = "";
+  const navigate = useNavigate();
 
-  // Item details based on the category
-  switch (category) {
-    case "Brownies":
-      itemName = "Brownies";
-      itemImagePath = "/images/brownies.jpg";
-      itemPriceLabel = "5555";
-      break;
-    case "BentoCakes":
-      itemName = "Bento Cake";
-      itemImagePath = "/images/bentocake.jpg";
-      itemPriceLabel = "7777";
-      break;
-    case "CupCakes":
-      itemName = "Cupcake";
-      itemImagePath = "/images/cupcakes.jpg";
-      itemPriceLabel = "8888";
-      break;
-    case "Muffins":
-      itemName = "Muffin";
-      itemImagePath = "/images/muffins.jpg";
-      itemPriceLabel = "9999";
-      break;
-    case "SwissRolls":
-      itemName = "Swiss Roll";
-      itemImagePath = "/images/swissrolls.jpg";
-      itemPriceLabel = "1111";
-      break;
-    default:
-      break;
-  }
-
-  const navigate = useNavigate(); // Hook for navigation
-
-  // Dummy ID
-  // const dummyId = "12345";
+  const [itemDetails, setItemDetails] = useState([]);
 
   const handlePaymentClick = () => {
-    navigate(`/${category}/${itemId}/payment`); // Navigate to the payment page
+    navigate(`/${category}/${itemId}/payment`);
   };
+
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      try {
+        const response = await axiosInstance.get(`/items/${category.toLowerCase()}/${itemId}`);
+        console.log("Brownie:", response.data);
+        setItemDetails(response.data.item);
+      } catch (error) {
+        console.log("Error Fetching Item Details:", error);
+      }
+    };
+    fetchItemDetails();
+    console.log("Item", itemDetails);
+  }, [category, itemId]);
 
   return (
     <div className="h-screen">
@@ -59,15 +38,19 @@ const Item = () => {
             <button onClick={handlePaymentClick} className="block w-full h-full">
               <div className="relative w-full h-full">
                 <img
-                  src={itemImagePath}
-                  alt={itemName}
+                  src={itemDetails.image}
+                  alt={itemDetails.name}
                   className="w-full h-full object-cover rounded-3xl"
                 />
                 <span className="absolute bottom-2 px-3 py-3 left-2 text-white">
-                  Price: {itemPriceLabel}
+                  Price: {itemDetails.price}
                 </span>
-                <span className="absolute bottom-2 px-3 py-3 right-2 text-white">Quantity:</span>
-                <span className="absolute bottom-2 px-3 py-3 left-32 text-white">Size:</span>
+                <span className="absolute bottom-2 px-3 py-3 right-2 text-white">
+                  Quantity:{itemDetails.quantity}
+                </span>
+                <span className="absolute bottom-2 px-3 py-3 left-32 text-white">
+                  Size:{itemDetails.size}
+                </span>
               </div>
             </button>
           </div>
@@ -78,3 +61,83 @@ const Item = () => {
 };
 
 export default Item;
+
+// import { Link, useNavigate, useParams } from "react-router-dom";
+// import InfoBar from "./InfoBar";
+
+// const Item = () => {
+//   const { category, itemId } = useParams();
+//   console.log(category, itemId);
+//   let itemName = "";
+//   let itemImagePath = "";
+//   let itemPriceLabel = "";
+
+//   // Item details based on the category
+//   switch (category) {
+//     case "brownies":
+//       itemName = "Brownies";
+//       itemImagePath = "/images/brownies.jpg";
+//       itemPriceLabel = "5555";
+//       break;
+//     case "bentocakes":
+//       itemName = "bento cake";
+//       itemImagePath = "/images/bentocake.jpg";
+//       itemPriceLabel = "7777";
+//       break;
+//     case "cupcakes":
+//       itemName = "cupcake";
+//       itemImagePath = "/images/cupcakes.jpg";
+//       itemPriceLabel = "8888";
+//       break;
+//     case "muffins":
+//       itemName = "muffin";
+//       itemImagePath = "/images/muffins.jpg";
+//       itemPriceLabel = "9999";
+//       break;
+//     case "swissrolls":
+//       itemName = "swiss roll";
+//       itemImagePath = "/images/swissrolls.jpg";
+//       itemPriceLabel = "1111";
+//       break;
+//     default:
+//       break;
+//   }
+
+//   const navigate = useNavigate(); // Hook for navigation
+
+//   // Dummy ID
+//   // const dummyId = "12345";
+
+//   const handlePaymentClick = () => {
+//     navigate(`/${category}/${itemId}/payment`); // Navigate to the payment page
+//   };
+
+//   return (
+//     <div className="h-screen">
+//       <InfoBar />
+//       <div className="m-auto mt-5 px-2 py-5 mb-5 pb-5  w-11/12 rounded-3xl">
+//         <div className="overflow-auto flex flex-wrap  mb-5 pb-5 ">
+//           <h3 className="text-center mt-5 mb-5 pt-2 pb-2 text-yellow-500 text-bold">{category}</h3>
+//           <div className="w-full pb-2 mb-2 p-1 h-64">
+//             <button onClick={handlePaymentClick} className="block w-full h-full">
+//               <div className="relative w-full h-full">
+//                 <img
+//                   src={itemImagePath}
+//                   alt={itemName}
+//                   className="w-full h-full object-cover rounded-3xl"
+//                 />
+//                 <span className="absolute bottom-2 px-3 py-3 left-2 text-white">
+//                   Price: {itemPriceLabel}
+//                 </span>
+//                 <span className="absolute bottom-2 px-3 py-3 right-2 text-white">Quantity:</span>
+//                 <span className="absolute bottom-2 px-3 py-3 left-32 text-white">Size:</span>
+//               </div>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Item;

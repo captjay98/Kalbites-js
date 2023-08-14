56c53a5f8e95fab9a8848213fe6f7da4a4ddefc1
captjay98Zorin <captjay98@gmail.com>
2023-08-14 23:05:56 +0100

    updated validators removed username and email for reg added phone number for login

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -3,15 +3,16 @@ import Joi from "joi";
 export const registerSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
-  userName: Joi.string().required(),
-  email: Joi.string().email().required(),
+  // userName: Joi.string().required(),
+  // email: Joi.string().email().required(),
   phone: Joi.required(),
   address: Joi.string().required(),
   password: Joi.string().min(8).required(),
+  rePassword: Joi.string().required(),
   account_type: Joi.string().valid("customer", "admin").required(),
 });
 
 export const loginSchema = Joi.object({
-  userName: Joi.string().required(),
+  phone: Joi.string().required(),
   password: Joi.string().required(),
 });

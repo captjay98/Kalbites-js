5ee9fc9fe48be56a015a17e23a057a741db81e04
captjay98Zorin <captjay98@gmail.com>
2023-08-14 23:08:06 +0100

    dunno

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,39 +1,37 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 const secret = "HelloWorld55";
 
-
 const getCurrentUser = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return next(); // continue without setting req.user
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next();
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   try {
     const decodedToken = jwt.verify(token, secret);
-    console.log('decodedToken:', decodedToken); // add logging statement
+    console.log("decodedToken:", decodedToken);
     req.user = decodedToken.userId;
-    console.log('req.user:', req.user); // add logging statement
+    console.log("req.user:", req.user);
     next();
   } catch (error) {
-    console.log(error)
+    console.log(error);
 
-    return next(); // continue without setting req.user
+    return next();
   }
 };
 
 export default getCurrentUser;
 
-
 function requireRole(role) {
-  return function(req, res, next) {
+  return function (req, res, next) {
     if (req.user && req.user.account_type === role) {
       next();
     } else {
       res.status(403).send("Forbidden");
     }
-  }
+  };
 }

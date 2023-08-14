8ac19088a7e2f1d14a2e214a266b1693b5a274b4
captjay98Zorin <captjay98@gmail.com>
2023-08-14 23:06:57 +0100

    Updated multer to be used for file upload

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,38 +1,15 @@
 import multer from "multer";
 
-export const upload = multer({
+const upload = multer({
   storage: multer.diskStorage({
-    destination: function(req, file, cb) {
-      let uploadPath = '';
-
-      if (file.fieldname === 'passport') {
-        uploadPath = `media/passport`;
-      } else if (file.fieldname === 'visas') {
-        uploadPath = `media/visa`;
-      } else if (file.fieldname === 'cv') {
-        uploadPath = `media/cv`;
-      }
-
-      cb(null, uploadPath);
+    destination: function (req, file, cb) {
+      cb(null, "./uploads");
     },
-    filename: function(req, file, cb) {
-      const fileType = file.mimetype.split('/')[1];
-      const filename = `${req.user.username}-${file.fieldname}.${fileType}`;
-
+    filename: function (req, file, cb) {
+      const filename = `${file.originalname}`;
       cb(null, filename);
-    }
+    },
   }),
-  fileFilter: function(req, file, cb) {
-    if (file.mimetype === 'application/pdf') {
-      cb(null, true);
-    } else {
-      cb(new Error('Only PDF files are allowed'));
-    }
-  }
-}).fields([
-  { name: 'cv', maxCount: 1 },
-  { name: 'passport', maxCount: 1 },
-  { name: 'visas', maxCount: 5 }
-]);
+});
 
-
+export default upload;

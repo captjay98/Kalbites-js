69119e5b802b227b07802da9bc1d72617e944477
captjay98 <captjay98@gmail.com>
2023-08-26 07:58:40 +0100

    done

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ import redisClient from "./middlewares/redisManager.js";
 const port = process.env.PORT || 3000;
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST"],
-  allowedHeaders: ["*"],
-};
-
-app.use(cors(corsOptions));
-
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://kalbites.vercel.app"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 dbConnect().then(() => {
   redisClient.connect();

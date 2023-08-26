736eab529cf856cfd834390206a89f5df00c6a31
captjay98 <captjay98@gmail.com>
2023-08-26 08:13:12 +0100

    using the same axios instance cu

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import InfoBar from "./InfoBar";
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3333",
-});
+import axiosInstance from "../../axiosInstance";
 
 const LoginForm = () => {
   const [user, LoginUser] = useState({
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,11 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import InfoBar from "./InfoBar";
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3333",
-});
+import axiosInstance from "../../axiosInstance";
 
 const RegisterForm = () => {
   const [user, registerUser] = useState({
